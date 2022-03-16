212837fca5f54cbfa973297d3972f1c08b9ac6cd
zb-zhangpengliang <zb-zhangpengliang@kingmed.com.cn>
2022-03-16 12:04:05 -0400

    updated: event and cards

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,18 +7,85 @@ sap.ui.define([
 
         onInit: function () {
             // set data model on view
-            var oData = {
+            const oData = {
                 recipient: {
                     name: "World"
-                }
+                },
+                scenario: [
+                    {
+                        label: "S/4 Upgrade",
+                        checked: false
+                    },
+                    {
+                        label: "S/4 Conversion",
+                        checked: false
+                    },
+                    {
+                        label: "BW4 HANA",
+                        checked: false
+                    },
+                    {
+                        label: "RCX",
+                        checked: false,
+                    },
+                    {
+                        label: "UDP",
+                        checked: false   
+                    }
+
+                ],
+                checks: [
+                    {
+                        label: "ABAP",
+                        selected: false,
+                        vaildXmls: [ 
+                            {
+                                type: "Normal",
+                                checked: true
+                            },
+                            {
+                                type: "Error",
+                                checked: false
+                            },
+                            {
+                                type: "No Data",
+                                checked: false
+                            },
+                        ]
+                    },
+                    {
+                        label: "Active Business Function",
+                        selected: false
+                    },
+                    {
+                        label: "Addons",
+                        selected: false
+                    },
+                    {
+                        label: "Interface",
+                        selected: false,
+                    },
+                    {
+                        label: "BSP",
+                        selected: false   
+                    }
+
+                ],
             };
             var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
+            this.getView().setModel(oModel, "scenarioModel");
         },
 
         onShowHello: function () {
             // show a native JavaScript alert
             alert("Hello World");
+        },
+
+        onScenarioSelect: function (oEvent) {
+            const oSource = oEvent.getSource();
+            if (oEvent.getParameters().selected) {
+                console.log(oSource.getText());
+            }
         }
     });
 });
